87077dec8265387bc220bed2d9350c5377b4b004
Wesley Douglas da Silva Tapajos <wesleytapajos@hotmail.com>
2024-07-05 14:39:49 -0400

    working with four verbs

---
+++ 02RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            
+        }
+
+    }
+}
